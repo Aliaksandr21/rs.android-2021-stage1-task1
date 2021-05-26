a4329aaf895a773968bc7eb76d31c28f25ac4a15
Aliaksandr21 <82441635+Aliaksandr21@users.noreply.github.com>
2021-05-26 23:16:29 +0300

    create : 1st task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
-    }
-}
+        if (sadArray.size <= 2) {
+        return sadArray
+            }
+        val oneArray = sadArray.reversedArray()
+        val pupokArray = arrayListOf(oneArray[0])
+        var left = 0
+
+        for (i in 1 until oneArray.lastIndex) {
+            if (oneArray[i] <= (oneArray[left] + oneArray[i + 1])) {
+                pupokArray.add(oneArray[i])
+                left = i
+            }
+        }
+        pupokArray.add(oneArray[oneArray.lastIndex])
+        return pupokArray.toIntArray().reversedArray()
+            }
+        }
+
